1080da3a74f88fe0a66d1213d3aa18ebc15ef9ad
Paulitchos <74797330+Paulitchos@users.noreply.github.com>
2022-12-05 03:13:31 +0000

    tweaks

--- Rental4You/Models/Empresa.cs
+++ Rental4You/Models/Empresa.cs
@@ -1,6 +1,14 @@
-﻿namespace Rental4You.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Rental4You.Models
 {
     public class Empresa
     {
+        public int Id { get; set; }
+        [PersonalData]
+        public string Nome { get; set; }
+
+        public Gestor gestor { get; set; }
+        public int GestorId { get; set; }
     }
 }
